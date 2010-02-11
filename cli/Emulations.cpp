2766f75558a27e995be632536d8666c918222c5e
Marc Ressl <mressl@gmail.com>
2010-02-11 12:46:22 +0000

    Emulation update

--- cli/Emulations.cpp
+++ cli/Emulations.cpp
@@ -7,37 +7,54 @@
  *
  */
 
+#ifdef WIN32
+#else
+
 #include <pthread.h>
+#endif
 
 #include "Emulations.h"
 #include "Emulation.h"
 
 vector<OEEmulation *> emulations;
+pthread_mutex_t emulationsMutex;
 
 void emulationsOpen()
 {
 	// Create mutex
-	
+	pthread_mutex_init(emulationsMutex, NULL);
 
 }
 
 void emulationsClose()
 {
-	
+	pthread_mutex_destroy(emulationsMutex);
 }
 
 
 OEEmulation *emulationAdd()
 {
-
 }
 
 bool emulationRemove(OEEmulation *emulation)
 {
-
 }
 
 bool emulationIoctl()
 {
 }
 
+bool emulationAddDevices(OEEmulation *emlation, string path)
+{
+	
+}
+
+bool emulationIsDeviceTerminal(OEEmulation *emlation, string path)
+{
+	
+}
+
+bool emulationRemoveDevice(OEEmulation *emlation, string path)
+{
+	
+}
