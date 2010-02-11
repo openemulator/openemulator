cdf7df8710df750a6730032db5ad30f8e7b867e3
Marc Ressl <mressl@gmail.com>
2010-02-11 12:34:35 +0000

    Emulation update

---
+++ cli/Emulations.cpp
@@ -0,0 +1,43 @@
+/*
+ *  Emulations.cpp
+ *  OpenEmulator
+ *
+ *  Created by Marc S. Reßl on 11/02/10.
+ *  Copyright 2010 ITBA. All rights reserved.
+ *
+ */
+
+#include <pthread.h>
+
+#include "Emulations.h"
+#include "Emulation.h"
+
+vector<OEEmulation *> emulations;
+
+void emulationsOpen()
+{
+	// Create mutex
+	
+
+}
+
+void emulationsClose()
+{
+	
+}
+
+
+OEEmulation *emulationAdd()
+{
+
+}
+
+bool emulationRemove(OEEmulation *emulation)
+{
+
+}
+
+bool emulationIoctl()
+{
+}
+
---
+++ cli/Emulations.h
@@ -0,0 +1,9 @@
+/*
+ *  Emulations.h
+ *  OpenEmulator
+ *
+ *  Created by Marc S. Reßl on 11/02/10.
+ *  Copyright 2010 ITBA. All rights reserved.
+ *
+ */
+
