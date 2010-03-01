98f4ce99ef2cfa73dc4cf8614ceb8c8313034e05
Marc Ressl <mressl@gmail.com>
2010-03-01 01:49:38 +0000

    Add Apple I

---
+++ libemulator/AppleI/AppleISystem.h
@@ -0,0 +1,30 @@
+
+/**
+ * libemulator
+ * Apple II System
+ * (C) 2010 by Marc S. Ressl (mressl@umich.edu)
+ * Released under the GPL
+ *
+ * Controls an Apple II system
+ */
+
+#include "OEComponent.h"
+
+// Messages
+enum
+{
+	APPLEII_EVENTS_SELECT_MAINCPU = OEIOCTL_USER,
+	APPLEII_EVENTS_SELECT_AUXCPU,
+	APPLEII_EVENTS_GET_SAMPLEINDEX,
+};
+
+// Receives HOST_AUDIO_BUFFERSTART
+// It is possible to configure one CPU
+// Calls the CPU
+// Translates sample and CPU clocks (clocksPerSample)
+// Receives IRQ and NMI requests, and routes them to the CPU
+// Receives HOST_EVENTS_RESET_UP and routes it to the CPU
+
+class AppleIIEvents : public OEComponent
+{
+};
