3d848e5805c6ef1d329bb404f2097bb71ada0644
Marc Ressl <mressl@gmail.com>
2010-03-01 01:49:10 +0000

    Added PIA682

--- libemulator/Common/PIA6821.cpp
+++ libemulator/Common/PIA6821.cpp
@@ -1,11 +1,92 @@
-/*
- *  PIA6821.cpp
- *  OpenEmulator
+
+/**
+ * libemulator
+ * PIA6821
+ * (C) 2010 by Marc S. Ressl (mressl@umich.edu)
+ * Released under the GPL
  *
- *  Created by Marc S. Reßl on 27/02/10.
- *  Copyright 2010 ITBA. All rights reserved.
- *
+ * Controls a generic 6821 Peripheral Interface Adapter
  */
 
 #include "PIA6821.h"
 
+PIA6821::PIA6821()
+{
+	reset();
+}
+
+void PIA6821::reset()
+{
+	controlRegisterA = 0;
+	dataDirectionRegisterA = 0;
+	dataRegisterA = 0;
+	controlRegisterB = 0;
+	dataDirectionRegisterB = 0;
+	dataRegisterB = 0;
+}
+
+int PIA6821::ioctl(int message, void *data)
+{
+	switch(message)
+	{
+		case OEIOCTL_CONNECT:
+		{
+			OEIoctlConnection *connection = (OEIoctlConnection *) data;
+			if (connection->name == "interfaceA")
+				interfaceA = connection->component;
+			else if (connection->name == "interfaceB")
+				interfaceB = connection->component;
+			else if (connection->name == "irqA")
+				irqA = connection->component;
+			else if (connection->name == "irqB")
+				irqB = connection->component;
+			break;
+		}
+		case PIA6821_RESET:
+		{
+			reset();
+			break;
+		}
+	}
+	
+	return 0;
+}
+
+int PIA6821::read(int address)
+{
+	switch(address & 0x3)
+	{
+		case PIA6821_RS_DATAREGISTERA:
+			if (controlRegisterA & PIA6821_CR_DATAREGISTER)
+			{
+				if (controlRegisterA)
+					break;
+				return dataRegisterA;
+			}
+			else
+				return dataDirectionRegisterA;
+		case PIA6821_RS_CONTROLREGISTERA:
+			return controlRegisterA;
+		case PIA6821_RS_DATAREGISTERB:
+			if (controlRegisterB & PIA6821_CR_DATAREGISTER)
+				return dataRegisterB;
+			else
+				return dataDirectionRegisterB;
+		case PIA6821_RS_CONTROLREGISTERB:
+			return controlRegisterB;
+	}
+	
+	return 0;
+}
+
+void PIA6821::write(int address, int value)
+{
+	switch(address & 0x3)
+	{
+		case PIA6821_RS_DATAREGISTERA:
+		case PIA6821_RS_CONTROLREGISTERA:
+		case PIA6821_RS_DATAREGISTERB:
+		case PIA6821_RS_CONTROLREGISTERB:
+			break;
+	}
+}
