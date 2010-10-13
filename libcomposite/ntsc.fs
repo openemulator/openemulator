a7305018c19acfa324aefa65c0ab03bc70f7046b
Marc Ressl <mressl@gmail.com>
2010-10-13 02:32:33 +0000

    shader update

--- libcomposite/ntsc.fs
+++ libcomposite/ntsc.fs
@@ -11,6 +11,15 @@ uniform float fsc;
 uniform float flicker;
 uniform float time;
 
+vec3 filter(vec2 q, vec2 qn, float i, float cy, float cc)
+{
+		vec3 p = texture2D(texture, vec2(qn.x + i / 512.0, qn.y)).xyz;
+		p.x *= cy;
+		p.y *= cc * sin(2.0 * 3.1415926535 * ((q.x + i) * fsc + hue)) * sqrt(2.0) * saturation;
+		p.z *= cc * cos(2.0 * 3.1415926535 * ((q.x + i) * fsc + hue)) * sqrt(2.0) * saturation;
+		return p;
+}
+
 // x, y, z is used as Y'UV
 void main(void)
 {
@@ -20,14 +29,23 @@ void main(void)
 	vec2 qn = q / 512.0;
 	qn.y = 1.0 - qn.y;
 	vec3 col = vec3(0.0, 0.0, 0.0);
-	for (float i = -11.0; i <= 11.0; i += 1.0)
-	{
-		vec3 p = texture2D(texture, vec2(qn.x + i / 512.0, qn.y)).xyz;
-		p.y *= sin(2.0 * 3.1415926535 * ((q.x + i) * fsc + hue)) * 2.0 * saturation;
-		p.z *= cos(2.0 * 3.1415926535 * ((q.x + i) * fsc + hue)) * 2.0 * saturation;
-		col += p;
-	}
-	col /= 23.0;
+	col += filter(q, qn, -8.0, 0.000000238419, 0.0);
+	col += filter(q, qn, -7.0, 0.000000043104, 0.0);
+	col += filter(q, qn, -6.0, 0.000000024426, 0.0);
+	col += filter(q, qn, -5.0, 0.000000021192, 0.0);
+	col += filter(q, qn, -4.0, 0.000000055881, 0.0);
+	col += filter(q, qn, -3.0, 0.000000024810, 0.0);
+	col += filter(q, qn, -2.0, 0.100000077592, 0.0);
+	col += filter(q, qn, -1.0, 0.200000037292, 0.0);
+	col += filter(q, qn, 0.0, 0.500000000000, 0.0);
+	col += filter(q, qn, -1.0, 0.200000037292, 0.0);
+	col += filter(q, qn, -2.0, 0.100000077592, 0.0);
+	col += filter(q, qn, -3.0, 0.000000024810, 0.0);
+	col += filter(q, qn, -4.0, 0.000000055881, 0.0);
+	col += filter(q, qn, -5.0, 0.000000021192, 0.0);
+	col += filter(q, qn, -6.0, 0.000000024426, 0.0);
+	col += filter(q, qn, -7.0, 0.000000043104, 0.0);
+	col += filter(q, qn, -8.0, 0.000000238419, 0.0);
 	col = decoderMatrix * col;
 	col = col * contrast + brightness;
 
