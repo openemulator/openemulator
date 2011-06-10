9a4bc96b2573553ed67946bf48a3c50679cbe54a
Marc Ressl <mressl@gmail.com>
2011-06-10 04:53:38 +0000

    Added Videx Videoterm

---
+++ libemulation/Implementation/Videx/VidexVideoterm.cpp
@@ -0,0 +1,15 @@
+
+/**
+ * libemulation
+ * Videx Videoterm
+ * (C) 2011 by Marc S. Ressl (mressl@umich.edu)
+ * Released under the GPL
+ *
+ * Controls a Videx Videoterm
+ */
+
+#include "VidexVideoterm.h"
+
+VidexVideoterm::VidexVideoterm()
+{
+}
---
+++ libemulation/Implementation/Videx/VidexVideoterm.h
@@ -0,0 +1,18 @@
+
+/**
+ * libemulation
+ * Videx Videoterm
+ * (C) 2011 by Marc S. Ressl (mressl@umich.edu)
+ * Released under the GPL
+ *
+ * Controls a Videx Videoterm
+ */
+
+#include "OEComponent.h"
+
+class VidexVideoterm : public OEComponent
+{
+public:
+	VidexVideoterm();
+	
+};
